1b3672cc471d8baacd50c7616b10a09042058796
kwrush <kaiwang.me@gmail.com>
2020-11-08 19:54:00 +0100

    fix(auth): update dropbox app key

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  clientId: process.env.CLIENT_ID || 'ssa1frswqq0sc2v',
+  clientId: process.env.CLIENT_ID || '8zxynlxwp7ec520',
   tokenKey: process.env.TOKEN_KEY || 'refresh-token',
   baseURL: process.env.BASE_URL || 'http://localhost',
   port: process.env.PORT || 3000,
